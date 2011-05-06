6d68eca9675c83f59282e32231e9f98532048d8d
Elio Schmutz <e.schmutz@4teamwork.ch>
2011-05-06 16:59:39 +0200

    added disable_border to request

--- ftw/usermanagement/browser/tabbed_management.py
+++ ftw/usermanagement/browser/tabbed_management.py
@@ -5,6 +5,12 @@ from ftw.usermanagement import user_management_factory as _
 class ManagementTabbedView(TabbedView):
     """TabbedView for User/Groups management"""
 
+    def __init__(self, context, request):
+        super(ManagementTabbedView, self).__init__(context, request)
+
+        if not request.get('disable_border'):
+            request.set('disable_border','1')
+
     def get_tabs(self):
         """Returns a list of dicts containing the tabs definitions"""
 
