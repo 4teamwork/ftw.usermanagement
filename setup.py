028219609cc5d0962ff28453f4777941ff411d1b
Jonas Baumann <jone@jone.ch>
2011-12-01 11:51:28 +0100

    Update package metadata.

--- setup.py
+++ setup.py
@@ -6,36 +6,42 @@ maintainer = 'Mathias Leimgruber'
 
 setup(name='ftw.usermanagement',
       version=version,
-      description="",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description='An advanced view for managing users in plone.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
-        "Framework :: Plone",
-        "Programming Language :: Python",
+        'Framework :: Plone',
+        'Framework :: Plone :: 4.1',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
         ],
-      keywords='',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw usermanagement user management view plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://svn.plone.org/svn/collective/',
-      license='GPL',
+      url='https://github.com/4teamwork/ftw.usermanagement/',
+      license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'plone.principalsource',
-          'collective.js.jqueryui',
-          'collective.js.ui.multiselect',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
 
+      install_requires=[
+        'setuptools',
+        'plone.principalsource',
+        'collective.js.jqueryui',
+        'collective.js.ui.multiselect',
+        # -*- Extra requirements: -*-
+        ],
+
+      entry_points='''
+      # -*- Entry points: -*-
       [z3c.autoinclude.plugin]
       target = plone
-      """,
+      ''',
       )
