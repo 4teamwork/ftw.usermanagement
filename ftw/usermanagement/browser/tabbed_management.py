d19113f094399966ee1d8619c16b483c3ad4ed86
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2011-12-23 15:03:17 +0100

    adde german translations for tabs

--- ftw/usermanagement/browser/tabbed_management.py
+++ ftw/usermanagement/browser/tabbed_management.py
@@ -9,16 +9,21 @@ class ManagementTabbedView(TabbedView):
         super(ManagementTabbedView, self).__init__(context, request)
 
         if not request.get('disable_border'):
-            request.set('disable_border','1')
+            request.set('disable_border', '1')
+        request.set('disable_plone.rightcolumn', True)
+        request.set('disable_plone.leftcolumn', True)
 
     def get_tabs(self):
         """Returns a list of dicts containing the tabs definitions"""
 
         translate = self.context.translate
         return [{'id':'users_management', 'class':'',
-                'description': translate(_('msg_usersDescription',default='Users-management')),
-                },
+                'description': translate(
+                    _(
+                        'msg_usersDescription',
+                         default='Users-management')), },
                 {'id':'groups_management', 'class':'',
-                'description': translate(_('msg_groupsDescription',default='Groups-management')),
-                },
-               ]
+                'description': translate(
+                    _(
+                        'msg_groupsDescription',
+                        default='Groups-management')), }, ]
