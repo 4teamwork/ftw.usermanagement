404cfb49c2aa58146687fde38e6cc637dd01e66f
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2012-04-02 16:38:14 +0200

    removed custom viewletmanager, changed browserlayer name

--- ftw/usermanagement/interfaces.py
+++ ftw/usermanagement/interfaces.py
@@ -1,11 +1,6 @@
 from plone.theme.interfaces import IDefaultPloneLayer
-from zope.viewlet.interfaces import IViewletManager
 
 
 class IFtwUserManagement(IDefaultPloneLayer):
     """Marker interface for a zope 3 browser layer.
     """
-    
-class IBeforTablerManager(IViewletManager):
-    """ViewletManager for Viewlets"""
-    
