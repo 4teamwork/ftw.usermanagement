f5872b5dc5c7f710eae3a1c8207a4f8ed679c72a
Jonas Baumann <jone@jone.ch>
2015-09-04 09:47:52 +0200

    cleanup and fix testing setup.

--- ftw/usermanagement/testing.py
+++ ftw/usermanagement/testing.py
@@ -31,27 +31,20 @@ class UsermanagementPloneLayer(PloneSandboxLayer):
     defaultBases = (PLONE_FIXTURE, )
 
     def setUpZope(self, app, configurationContext):
-        # Load ZCML
-        import ftw.usermanagement
-        xmlconfig.file(
-            'configure.zcml', ftw.usermanagement, context=configurationContext)
+        xmlconfig.string(
+            '<configure xmlns="http://namespaces.zope.org/zope">'
+            '  <include package="z3c.autoinclude" file="meta.zcml" />'
+            '  <includePlugins package="plone" />'
+            '  <includePluginsOverrides package="plone" />'
+            '</configure>',
+            context=configurationContext)
+
         installProduct(app, 'ftw.usermanagement')
-
-        import ftw.tabbedview
-        xmlconfig.file(
-            'configure.zcml', ftw.tabbedview, context=configurationContext)
-        installProduct(app, 'ftw.tabbedview')
-
-        import ftw.table
-        xmlconfig.file(
-            'configure.zcml', ftw.table, context=configurationContext)
         installProduct(app, 'ftw.table')
 
     def setUpPloneSite(self, portal):
-        # Install into Plone site using portal_setup
         applyProfile(portal, 'ftw.usermanagement:default')
-        applyProfile(portal, 'ftw.tabbedview:default')
-        applyProfile(portal, 'ftw.table:default')
+
 
 UsermanagementPloneFixture = UsermanagementPloneLayer()
 USERMANAGEMENT_PLONE_LAYER = IntegrationTesting(
